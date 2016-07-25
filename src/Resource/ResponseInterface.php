8ee2d76feb055931e12fbb8d614462d7c1df3f2c
bbos <bostjan@preskok.si>
2016-07-25 22:49:06 +0200

    param fix

--- src/Resource/ResponseInterface.php
+++ src/Resource/ResponseInterface.php
@@ -11,7 +11,10 @@ interface ResponseInterface
      * Parse response data and return object
      * @param \stdClass $data
      */
-    public function parse($data);
+    public function parse(\stdClass $data);
 
+    /**
+     * @return array
+     */
     public function getData();
 }
